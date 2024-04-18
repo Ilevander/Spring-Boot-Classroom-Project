853b8d23dc37dd32439dae5b23012a6a809793f7
dell <dell@DESKTOP-4P8U8AR>
2024-04-18 23:48:11 +0100

    customizing Assignment Model to generate table in Db using JPA ,Hibernate

--- src/main/java/com/ilyass/web/models/Assignment.java
+++ src/main/java/com/ilyass/web/models/Assignment.java
@@ -1,8 +1,8 @@
 package com.ilyass.web.models;
 
-
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,21 +11,31 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
-@Table(name="assignments")
+@Table(name = "assignments")
 public class Assignment {
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private int assignmentId;
-	    private String assignmentName;
-	    private String description;
-	    private Date deadline;
-	    private int points;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int assignmentId;
+
+    @Column(name = "assignment_name")
+    private String assignmentName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "deadline")
+    private Date deadline;
+
+    @Column(name = "points")
+    private int points;
 }
