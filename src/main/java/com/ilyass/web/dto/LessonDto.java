135d512919d5df2f8b173c6eb412b7e03793f4f6
dell <dell@DESKTOP-4P8U8AR>
2024-04-21 16:20:03 +0100

    Creating MessageDto DTO

---
+++ src/main/java/com/ilyass/web/dto/LessonDto.java
@@ -0,0 +1,19 @@
+package com.ilyass.web.dto;
+
+import com.ilyass.web.models.Resource;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class LessonDto {
+
+    private int lessonId;
+    private String lessonName;
+    private String description;
+    private int duration;
+    private List<Resource> materials;
+
+}
