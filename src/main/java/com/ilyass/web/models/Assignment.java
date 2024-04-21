6ee0547f2e0fc9790c85fa35a3581b0a4c9da8c8
dell <dell@DESKTOP-4P8U8AR>
2024-04-21 22:51:59 +0100

    Creating the Assignment Model , adding @Data to use Lombok on Mapper

--- src/main/java/com/ilyass/web/models/Assignment.java
+++ src/main/java/com/ilyass/web/models/Assignment.java
@@ -11,12 +11,10 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
