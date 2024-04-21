db931fff489e0e74d827eff23e98ded010c9f1a2
dell <dell@DESKTOP-4P8U8AR>
2024-04-21 22:49:02 +0100

    Creating the Assignment Mapper

---
+++ src/main/java/com/ilyass/web/mapper/AssignmentMapper.java
@@ -0,0 +1,34 @@
+package com.ilyass.web.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+
+import com.ilyass.web.dto.AssignmentDto;
+import com.ilyass.web.models.Assignment;
+
+public class AssignmentMapper {
+
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+    public static Assignment mapToAssignment(AssignmentDto assignmentDto) {
+        return modelMapper.map(assignmentDto, Assignment.class);
+    }
+
+    public static AssignmentDto mapToAssignmentDto(Assignment assignment) {
+        return modelMapper.map(assignment, AssignmentDto.class);
+    }
+
+    public static List<Assignment> mapToAssignments(List<AssignmentDto> assignmentDtos) {
+        return assignmentDtos.stream() 
+                .map(dto -> modelMapper.map(dto, Assignment.class))
+                .collect(Collectors.toList());
+    }
+ 
+    public static List<AssignmentDto> mapToAssignmentDtos(List<Assignment> assignments) {
+        return assignments.stream()
+                .map(entity -> modelMapper.map(entity, AssignmentDto.class))
+                .collect(Collectors.toList());
+    }
+}
