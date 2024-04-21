17a236d386c6d19046320a26ae5b25d9a3ccdc71
dell <dell@DESKTOP-4P8U8AR>
2024-04-21 21:22:55 +0100

    Updating Assessment Service Implementation to use the Assessment Mapper

--- src/main/java/com/ilyass/web/services/Impl/AssessmentServiceImpl.java
+++ src/main/java/com/ilyass/web/services/Impl/AssessmentServiceImpl.java
@@ -2,44 +2,52 @@ package com.ilyass.web.services.Impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.ilyass.web.dto.AssessmentDto;
-import com.ilyass.web.services.AssessmentService;
+import com.ilyass.web.mapper.AssessmentMapper;
+import com.ilyass.web.models.Assessment;
 import com.ilyass.web.repositories.AssessmentRepository;
+import com.ilyass.web.services.AssessmentService; 
 
-
+@Service
 public class AssessmentServiceImpl implements AssessmentService {
 
-	private AssessmentRepository AssessmentRepository;
-	
-	
-	@Override
-	public List<AssessmentDto> findAllAssessments() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private final AssessmentRepository assessmentRepository;
 
-	@Override
-	public AssessmentDto findAssessmentById(int assessmentId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Autowired
+    public AssessmentServiceImpl(AssessmentRepository assessmentRepository) {
+        this.assessmentRepository = assessmentRepository;
+    }
 
-	@Override
-	public List<AssessmentDto> findAssessmentsByType(String type) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<AssessmentDto> findAllAssessments() {
+        List<Assessment> assessments = assessmentRepository.findAll();
+        return AssessmentMapper.mapToAssessmentDtos(assessments); 
+    }
 
-	@Override
-	public List<AssessmentDto> findAssessmentsByDuration(int duration) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public AssessmentDto findAssessmentById(int assessmentId) {
+        Assessment assessment = assessmentRepository.findById(assessmentId)
+                .orElseThrow(() -> new RuntimeException("Assessment not found"));
+        return AssessmentMapper.mapToAssessmentDto(assessment); 
+    }
 
-	@Override
-	public void deleteAssessment(int assessmentId) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public List<AssessmentDto> findAssessmentsByType(String type) {
+        List<Assessment> assessments = assessmentRepository.findByType(type);
+        return AssessmentMapper.mapToAssessmentDtos(assessments);
+    }
 
+    @Override
+    public List<AssessmentDto> findAssessmentsByDuration(int duration) {
+        List<Assessment> assessments = assessmentRepository.findByDuration(duration);
+        return AssessmentMapper.mapToAssessmentDtos(assessments); 
+    }
+
+    @Override
+    public void deleteAssessment(int assessmentId) {
+        assessmentRepository.deleteById(assessmentId);
+    }
 }
